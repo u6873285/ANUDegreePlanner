5aa2aa0115e2a80de8063dd37980ae2846202443
ray <raymondrj123@gmail.com>
2023-08-22 13:29:15 +1000

    switch to mongodb

--- api/index.js
+++ api/index.js
@@ -1,45 +1,51 @@
 const express = require('express')
-const Pool = require('pg').Pool
+// const Pool = require('pg').Pool
+
+const { MongoClient } = require("mongodb")
+const uri = "mongodb://127.0.0.1:27017/" // mongodb://localhost:27017 is address for local install
+const client = new MongoClient(uri)
+
+
+// let conn = client.connect();
+let db = client.db("yo");
 
 const app = express()
 const port = 3000
-
-const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost', // change this to aws endpoint
-  database: 'postgres', // change to degree planner - idk name yet - course_data
-  password: 'test', // aws password is 'degreeplanner'
-  port: 5432,
-})
 
 app.listen(port, () => {
 console.log(`App running on port ${port}.`)
 })
 
 
-// initial endponts to be changed once database is fully set up
-// currently it is pointed to local postgres server
+// // initial endponts to be changed once database is fully set up
+// // currently it is pointed to local postgres server
 
 app.get('/', (request, response) => {
-    response.send('Node.js, Express, and Postgres API \n wassup' )
+    response.send('Node.js, Express, and Postgres API nvm its actually mongodb \n wassup' )
   })
 
+app.get("/course=:id", async (req, res) => {
+    let id = req.params.id
+    pars = parseInt(pars)
+    // console.log(pars)
+    let collection = await db.collection("uhm2");
+    let query = {Course_ID: id};
+    let result = await collection.findOne(query);
+  
+    if (!result) res.send("Not found").status(404);
+    else res.send(result).status(200);
+  });
 
-app.get("/yo", (req, res) => {
-    pool.query("SELECT * FROM course_data WHERE subject = 'COMP' LIMIT 2;", (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.send(results.rows)
-})})
+app.get("/yo", async (req, res) => {
+    let collection = await db.collection("uhm2");
+    let query = {Subject: "COMP"};
+    let result = await collection.find(query).limit(3);
+    let results_list = [];
+    for await (const doc of result){
+        results_list.push(doc)
+    }
 
-app.get("/yo/:num", (req, res) => {
-    var num = req.params.num
-    num.replace(":",'') // this is needed for some reason when using the variable in the query
-    num = parseInt(num)
-    pool.query( `SELECT * FROM course_data WHERE subject = 'COMP' LIMIT ${num}`, (error, results) => {
-        if (error) {
-            throw error
-        }
-        res.send(results.rows)
-})})
+    if (!result) res.send("Not found").status(404);
+    else res.send(results_list).status(200);
+
+})
